35996f323cedb4590daaabc10dd766f1358bef6b
Alex Markessinis <markea125@gmail.com>
2018-09-13 15:13:55 -0400

    backups are placed in a sub directory in the repo

--- EsxiBackup.ps1
+++ EsxiBackup.ps1
@@ -1,9 +1,13 @@
 # Read the config
 $config = (Get-Content config.json) -join "`n" | ConvertFrom-Json
 
+# Create a new backup destination folder
+$repo = Join-Path $config.backup_repo (get-date).ToString('yyyy-MM-dd_hh-mm-ss-tt');
+New-Item -ItemType directory -Path $repo -Force;
+
 # Run the backup
 foreach ($h in $config.hosts) {
     Connect-VIServer $h.address -user $h.user -password $h.pass;
-    Get-VMHostFirmware -vmhost $h.address -BackupConfiguration -DestinationPath $config.backup_repo;
+    Get-VMHostFirmware -vmhost $h.address -BackupConfiguration -DestinationPath $repo;
     Disconnect-VIServer $h.address -Force -Confirm:$false;
 }
