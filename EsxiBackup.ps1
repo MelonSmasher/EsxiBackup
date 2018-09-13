6454b6a8442dab25614c277e5071600d40a59bcd
Alex Markessinis <markea125@gmail.com>
2018-09-13 15:47:49 -0400

    if the backup repo path is not defined in the config the default repo is used

--- EsxiBackup.ps1
+++ EsxiBackup.ps1
@@ -2,8 +2,9 @@
 $config = (Get-Content (Join-Path $PSScriptRoot config.json)) -join "`n" | ConvertFrom-Json
 
 # Create a new backup destination folder
+if (!$config.backup_repo) { $config.backup_repo = (Join-Path $PSScriptRoot 'Backups'); }
 $repo = Join-Path $config.backup_repo (get-date).ToString('yyyy-MM-dd_hh-mm-ss-tt');
-New-Item -ItemType directory -Path $repo -Force;
+New-Item -ItemType directory -Path $repo -Force -Recurse;
 
 # Run the backup
 foreach ($h in $config.hosts) {
